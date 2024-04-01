9e514b79223647c7d23641ef7106a6568dee9ed6
connorq <connorquitt@gmail.com>
2024-04-01 18:53:23 -0500

    all tests passing

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'M', 'brown', 'nice', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'F', 'black', 'mean', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 6, 'M', 'white', 'nice', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 8, 'M', 'grey', 'mean', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 89, 'F', 'brown', 'nice', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 9, 'M', 'black', 'nice', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 8, 'F', 'brown', 'nice', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 78, 'M', 'black', 'mean', false);
